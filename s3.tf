696e1a5fe7af27929daef9f40f12f3eb167dd6bf
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-20 11:45:12 +0000

    s3 bucket

---
+++ s3.tf
@@ -0,0 +1,18 @@
+# ---------------------------------------
+# s3 Bucket
+# ---------------------------------------
+
+resource "aws_s3_bucket" "b" {
+  bucket_prefix = "${var.environment}-bastion"
+  acl           = "private"
+  versioning {
+    enabled = "true"
+  }
+
+
+  tags = {
+    Environment   = "${var.environment}"
+    orchestration = "${var.orchestration}"
+    purpose       = "${var.environment}-bastion-data"
+  }
+}
